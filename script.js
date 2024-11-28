cd6b58c54141c5988a2e38009f473197238527ae
shaunP-2003 <151203889+shaunP-2003@users.noreply.github.com>
2024-11-29 00:39:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".mobile-menu-links")
+    const icon = document.querySelector(".m-nav-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}    
